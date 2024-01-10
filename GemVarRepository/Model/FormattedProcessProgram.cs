ff2922fa82237d2dd42d806e16d5e14bad1915dd
張桂誠 <greg4253058@gmail.com>
2024-01-10 13:49:25 +0800

    決定用ef+dapper處理json

--- GemVarRepository/Model/FormattedProcessProgram.cs
+++ GemVarRepository/Model/FormattedProcessProgram.cs
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 namespace GemVarRepository.Model;
 public class FormattedProcessProgram
 {
-    public Guid Id { get; set; }
+    /// <summary>
+    /// or PPNAME ?
+    /// </summary>
     public string PPID {  get; set; }
-    public DateTime UpdateTime { get; set; }
 
-    public List<ProcessParameter> ProcessParameters { get; set; } = new();
+    //更多資訊, Recipe Specifier
+
+    public DateTime UpdateTime { get; set; }
+    /// <summary>
+    /// For PPChangeStatus, 1 Created, 2 Edited, 3 Deleted , 4-64 Reserved
+    /// </summary>
+    public int Status { get; set; }
+    /// <summary>
+    /// JSON !!!
+    /// </summary>
+    public string PPBody { get; set; }
+    //public List<ProcessParameter> ProcessParameters { get; set; } = new();
+    public string? Editor { get; set; }
+    public string? Description { get; set; }
+    public string? ApprovalLevel { get; set; }
 }
